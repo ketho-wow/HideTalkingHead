dcacb092a412059bd8b51262258d77ab6810bf96
Ketho <sghs17@outlook.com>
2016-08-14 07:44:53 +0200

    Keep the voice line sounds active (thanks q3fuba!)

--- HideTalkingHead.lua
+++ HideTalkingHead.lua
@@ -1,1 +1,15 @@
-UIParent:UnregisterEvent("TALKINGHEAD_REQUESTED")
+-- License: Public Domain
+
+local f = CreateFrame("Frame")
+
+function f:OnEvent(event, addon)
+	if addon == "Blizzard_TalkingHeadUI" then
+		hooksecurefunc("TalkingHeadFrame_PlayCurrent", function()
+			TalkingHeadFrame:Hide()
+		end)
+		self:UnregisterEvent(event)
+	end
+end
+
+f:RegisterEvent("ADDON_LOADED")
+f:SetScript("OnEvent", f.OnEvent)
